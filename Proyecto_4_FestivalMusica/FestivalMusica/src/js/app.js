cc683eaace2abfb1577b40bf90e75da7317f1d71
sunsitox <josemiguel.mogo@gmail.com>
2024-04-27 17:44:32 -0400

    proyecto_4
    proyecto_4

--- Proyecto_4_FestivalMusica/FestivalMusica/src/js/app.js
+++ Proyecto_4_FestivalMusica/FestivalMusica/src/js/app.js
@@ -24,9 +24,12 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
 
     for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
-        const imagen = document.createElement('IMG')
-        imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = 'Imagen Galería'
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+            <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+            <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+            <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+        `;
 
         // Event Handler
         imagen.onclick = function() {
@@ -38,9 +41,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-    const imagen = document.createElement('IMG')
-    imagen.src = `src/img/gallery/full/${i}.jpg`
-    imagen.alt = 'Imagen Galería'
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+    `;
 
     // Generar Modal
     const modal = document.createElement('DIV')
